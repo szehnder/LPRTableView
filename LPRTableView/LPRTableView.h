954b11746fa193f71db39216e5d284911e74c025
Sean Zehnder <seanzehnder@gmail.com>
2016-02-19 09:18:16 -0500

    repackaged as a cocoapod for easier dependency management

---
+++ LPRTableView/LPRTableView.h
@@ -0,0 +1,19 @@
+//
+//  LPRTableView.h
+//  LPRTableView
+//
+//  Created by Sean Zehnder on 2/19/16.
+//  Copyright © 2016 nicolasgomollon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LPRTableView.
+FOUNDATION_EXPORT double LPRTableViewVersionNumber;
+
+//! Project version string for LPRTableView.
+FOUNDATION_EXPORT const unsigned char LPRTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPRTableView/PublicHeader.h>
+
+
